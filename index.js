c7015da524bbfb0ebfff14e8ed7a380ade373d58
Vova Melnyk <hagmen4339@gmail.com>
2022-03-30 21:56:23 +0300

    finish lesson 9

--- index.js
+++ index.js
@@ -1,3 +1,90 @@
+// const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// const array2 = new Array(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
+"use strict";
+
+// 1) обыкновенная ф-я
+// 2) Стрелочная ф-я
+
+// const user = {
+//   name: "John",
+//   age: 30,
+//   getAge: function () {
+//     // let this = user;
+//     // return user.age;
+//     // return this.age; // user.age
+//     console.log(this);
+
+//     const arrowThis = () => {
+//       console.log("arrow", this); //user
+//     };
+
+//     arrowThis();
+//   },
+
+// getName: () => {
+//   // let this = пусто
+//   console.log(this);
+// },
+// };
+
+// console.log(user.getAge());
+// user.getName();
+
+// window.showThis = function showThis() {
+//   console.log(this);
+// };
+
+// function showThis() {
+//   console.log(this);
+// }
+
+// const arrowThis = () => {
+//   console.log(this);
+// };
+
+// // showThis();
+// arrowThis();
+
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// const result = numbers.filter((number) => number < 5);
+// const filterNumbers = (number) => number < 5;
+// const filterNumbersGreater = (number) => number > 5;
+// numbers.filter(filterNumbers);
+// numbers.filter(filterNumbersGreater);
+
+// console.log(result);
+
+// const user = {
+//   name: "John",
+//   getName: function (devider, deviderEnd) {
+//     return `${devider} ${this.name} ${deviderEnd}`;
+//   },
+// };
+
+// const hotel = {
+//   name: "Resort Hotel",
+// };
+
+// console.log(user.getName());
+// console.log(hotel.getName());
+
+// call - coma
+// apply - array
+
+// console.log(user.getName.apply(hotel, ["!", "&"]));
+// console.log(user.getName.call(hotel, "?", "*"));
+
+// const bindedGetName = user.getName.bind(hotel);
+
+// console.log(bindedGetName("?", "*"));
+
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// find largest number in array of numbers
+// const maxNumber = Array.prototype.call(Math.max(numbers));
+
 // # Модуль 5. Занятие 9. Контекст вызова функции и this
 
 // ## Example 1 - Мастерская драгоценностей
@@ -82,3 +169,33 @@
 // - `gender - строка
 // - getFullName - метод
 // - getAge - метод
+
+function User(name, surname, age, gender) {
+  // this = {}
+  this.name = name;
+  this.surname = surname;
+  this.age = age;
+  this.gender = gender;
+  // return this
+}
+
+User.prototype.getFullName = function () {
+  return `${this.name} ${this.surname}`;
+};
+
+User.prototype.getAge = function () {
+  return this.age;
+};
+
+const user = new User("Bob", "Dou", "12", "male");
+const user2 = new User("John", "Malkovich", "25", "male");
+
+console.log(user);
+console.log(user2);
+
+console.log(user.getFullName());
+console.log(user2.getAge());
+
+const numbers = [1, 2, 3, 4, 5];
+
+console.log(numbers);
