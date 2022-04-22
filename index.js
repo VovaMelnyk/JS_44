a75d0b1b54524b5c921c19f0649fdfb36b3ae347
Vova Melnyk <hagmen4339@gmail.com>
2022-04-22 19:26:19 +0300

    add module 7

--- index.js
+++ index.js
@@ -1,136 +1,95 @@
-// Задание №1 создать модальное окно которое будет появляться при нажатии на кнопку
-// и закрываться при нажатии на кнопку внутри модалки
+// Задание №1 Написать код где при навидении на кнопку она меняет свои координаты на случайные
 
-// const refs = {
-//   openModalBtn: document.querySelector(".open-modal"),
-//   overlay: document.querySelector(".overlay"),
-//   closeModalBtn: document.querySelector(".close"),
+// const test = document.querySelector("#test");
+
+// window.addEventListener("click", () => console.log("aaaaaaaa"));
+// window.addEventListener("click", () => console.log("bbbbbbb"));
+
+// let container = document.querySelector(".container");
+// function changeCords(e) {
+//   let btnHeight = e.target.clientHeight;
+//   let btnWidth = e.target.clientWidth;
+//   let newTop = Math.floor(Math.random() * (window.innerHeight - btnHeight));
+//   let newLeft = Math.floor(Math.random() * (window.innerWidth - btnWidth));
+//   e.target.style.top = `${newTop}px`;
+//   e.target.style.left = `${newLeft}px`;
+// }
+// container.addEventListener("mouseover", changeCords);
+
+// add lodash and use throttle and debounce
+
+// Оптимизировать роботу scroll и resize
+
+// function scrollTrack() {
+//   console.log("scroll");
+// }
+
+// function resizeTrack() {
+//   console.log("resize");
+// }
+
+// const scrollThrottled = _.throttle(scrollTrack, 500); // create new function
+
+// addEventListener("scroll", scrollThrottled);
+// addEventListener("scroll", _.throttle(scrollTrack, 500));
+
+// addEventListener("resize", _.throttle(resizeTrack, 700));
+
+// function trackInputEvent() {
+//   console.log("Send data to server");
+// }
+
+// const input = document.querySelector("input");
+
+// input.addEventListener("input", _.debounce(trackInputEvent, 1000));
+
+// Оптимизировать роботу события input
+
+// const sendDataToServer = function () {
+//   console.log("server call");
+//   console.log(input.value);
 // };
 
-// const toggleModal = () => {
-//   refs.overlay.classList.toggle("open");
+// const debounceSendDataToServer = _.debounce(sendDataToServer, 300);
+// const input = document.querySelector("input");
+
+// input.addEventListener("input", debounceSendDataToServer);
+
+
+// Написать свое TODO приложение
+
+
+
+// Написать сайт на котором при загрузке проигрвывается видео. Как только видео пропадает
+// из области видимостя более чем на 60% - ставить его на паузу
+
+// const videoPlayer = document.querySelector("video");
+
+// const handleVideoObserver = () => {
+//   if (!videoPlayer.paused) {
+//     videoPlayer.pause();
+//   } else {
+//     videoPlayer.play();
+//   }
 // };
 
-// refs.openModalBtn.addEventListener("click", toggleModal);
-// refs.closeModalBtn.addEventListener("click", toggleModal);
+// const watcher = new IntersectionObserver(handleVideoObserver, {
+//   threshold: 0.5,
+// });
 
-// const openModal = () => {
-//   refs.overlay.classList.add("open");
-// };
+// watcher.observe(videoPlayer);
 
-// const closeModal = () => {
-//   refs.overlay.classList.remove("open");
-// };
+// const video = document.querySelector("video");
 
-// refs.openModalBtn.addEventListener("click", openModal);
-// refs.closeModalBtn.addEventListener("click", closeModal);
-
-// const openModal = () => {
-//   refs.overlay.style.display = "block";
-// };
-
-// const closeModal = () => {
-//   refs.overlay.style.display = "none";
-// };
-
-// refs.openModalBtn.addEventListener("click", openModal);
-// refs.closeModalBtn.addEventListener("click", closeModal);
-
-// const refs = {
-//   openModalBtn: document.querySelector(".open-modal"),
-//   body: document.querySelector("body"),
-// };
-
-// const overlay = document.createElement("div");
-// const modal = document.createElement("div");
-
-// const modalContent = `<h1 class="title">Title</h1>
-//             <p class="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas quos sed blanditiis, optio
-//                 molestiae reprehenderit mollitia earum repellat vitae iure magnam, facilis eveniet minima excepturi.
-//                 Illo aliquid eligendi dolor vitae.</p>
-//             <button class="close">Close</button>`;
-
-// overlay.classList.add("overlay");
-// modal.classList.add("modal");
-
-// overlay.appendChild(modal);
-// refs.body.prepend(overlay);
-
-// modal.insertAdjacentHTML("beforeend", modalContent);
-
-// refs.closeBtn = document.querySelector(".close");
-// refs.overlay = document.querySelector(".overlay");
-
-// const toggleModal = () => {
-//   refs.overlay.classList.toggle("open");
-// };
-
-// refs.openModalBtn.addEventListener("click", toggleModal);
-// refs.closeModalBtn.addEventListener("click", toggleModal);
-
-// console.log(refs);
-
-// Задание №2 написать калькулятор стоимости продукта
-
-const refs = {
-  priceInput: document.querySelector("#price"),
-  quantityInput: document.querySelector("#quantity"),
-  total: document.querySelector(".total"),
-  form: document.querySelector(".form"),
-  amount: document.querySelector(".amount"),
-  increase: document.querySelector("#increase"),
-};
-
-const formHandler = () => {
-  refs.total.textContent = countTotal(refs.priceInput, refs.quantityInput);
-  refs.amount.textContent = refs.quantityInput.value;
-};
-
-const countTotal = (priceInput, quantityInput) => {
-  const price = priceInput.value;
-  const quantity = quantityInput.value;
-  const total = (price * quantity).toFixed(2);
-  if (isNaN(total)) {
-    return `Введите корректные данные`;
-  }
-  return `${price * quantity} грн`;
-};
-
-const increaseHandler = () => {
-  let inputMax = Number(refs.quantityInput.max);
-  inputMax += 5;
-  refs.quantityInput.max = inputMax;
-  refs.quantityInput.value = inputMax;
-  formHandler();
-
-  console.log(refs.quantityInput.max);
-};
-
-refs.form.addEventListener("input", formHandler);
-refs.increase.addEventListener("click", increaseHandler);
-
-// Задание №3 Написать код где при навидении на кнопку она меняет свои координаты на случайные
-
-// Задание #4 Сгенерировать сайт используя JS
-
-// const phonebook = {
-//   contacts: [],
-//   add(contact) {
-//     const newContact = {
-//       list: "default",
-//       ...contact,
-//       id: this.generateId(),
-//       createdAt: getDate(),
-//     };
-//     contacts.push(newContact);
+// let observer = new IntersectionObserver(
+//   () => {
+//     if (!video.paused) {
+//       video.pause();
+//     } else {
+//       video.play();
+//     }
 //   },
-//   generateId() {
-//     console.log("_" + Math.random().toString(36).substr(2, 9));
-//     return "_" + Math.random().toString(36).substr(2, 9);
-//   },
-//   getDate() {
-//     return Date.now();
-//   },
-// };
+//   { threshold: 0.6 }
+// );
 
-// phonebook.add({});
+// observer.observe(video);
