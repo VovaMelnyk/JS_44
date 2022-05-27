6a25384884477a1b6375515000b59aec8127315e
Vova Melnyk <hagmen4339@gmail.com>
2022-05-27 22:08:05 +0300

    finish lesson

--- index.js
+++ index.js
@@ -1,21 +1,29 @@
+// try {
+//   JSON.parse("test");
+// } catch (error) {
+//   //   throw new Error("test");
+// }
+
+// JSON.parse("test");
+
+// fetch().then().then().catch().finally(Отключить лоадер);
+
 // $teaser
 // let string = 'credit card number: 1234-2521-2315 credit card number2: 1234-2521-5897 ',
 //   regV = /\b[0-9]{4}-[0-9]{4}-[0-9]{4}\b/g;
 //
 // console.log(string.match(regV));
 
-
 // $literals
 // let reg = new RegExp();
 // let pattern = /win/;
-// const string = 'text windows win';
-// console.log(string.match(pattern));
+// const string = "text windows win";
+// // console.log(string.match(pattern));
 // console.log(pattern.test(string));
 
-
 // $flags
-// let pattern = /win/ig;
-// const string = 'text Windows win';
+// let pattern = /x/img;
+// const string = "text Windows win";
 // console.log(string.match(pattern));
 // let string = 'Is that all there is?',
 //     reg = /is/gi;
@@ -24,6 +32,30 @@
 
 // $classes
 // let string = 'Q5 wq w. www h - wh3whwh';
+
+// const phone = "0876542312";
+// const phone = "Q5 wq w. www h - wh3whwh";
+// const phone = "Hello hel qeqweqwe 089 ererrwsfd 56 djgkdjfgkdfgj 7 @";
+
+// const pattern = /\d{1,3}/g; // 0 - 9
+// const pattern = /\d{2}/g; // 0 - 9
+// const pattern = /\D{1,}/g; // 0 - 9
+
+// const pattern = /(\border\b|\bbuy\b)/gi; // 0 - 9
+// const pattern = /[0-9a-zA-Z@]/gi // 0 - 9;
+// const pattern = /\bQ[a-zA-Z]\+$\b/gi; // 0 - 9;
+
+
+// const patern = /^(http|https)/;
+// const patern = /^(http(s)?)/
+
+
+// const fileExt = /\.txt$/;
+// if (fileExt.test(file.name)) {
+//   console.log("txt file");
+// }
+// console.log(phone.match(pattern));
+
 
 // let b = /[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/ig
 
@@ -44,7 +76,6 @@
 // //  regV = /^\w+@[a-z]+\.[a-z.]{3}$/g;
 // console.log(string.match(regV));
 // console.log(regV.test(string));
-
 
 // console.log(string.match(regV));
 
@@ -67,7 +98,7 @@
 // let str = 'The car par ked in the gar age.';
 // let regV = /\b[^c]ar\b/g;
 // console.log(str.match(regV));
-// /^\w+\.c$/ - '.c' 
+// /^\w+\.c$/ - '.c'
 // console.log(str.match(regV));
 // +
 // let regV = /\d+/ig;
@@ -84,7 +115,6 @@
 // const reg = /Java(?=script)/;
 // console.log(str.match(reg));
 // console.log(atr.match(reg));
-
 
 // let test = `Card Number : 4906962001841822
 // Expiration : 02/2020
